a32926f03691cd7080a6bb0a872b246d646e38d8
PC <mohamedgamal03776@gmail.com>
2026-06-17 19:52:03 +0300

    v8

--- src/main/java/com/mo/MG_Movie/config/SecurityConfig.java
+++ src/main/java/com/mo/MG_Movie/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://mg-movie.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://mg-movie.vercel.app", "https://mg-movie-front-end-lp46.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
         configuration.setExposedHeaders(List.of("Authorization"));
