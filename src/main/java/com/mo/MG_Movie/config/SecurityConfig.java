cc0c3c901f96a083cd6d90e9496ae60cf558e671
PC <mohamedgamal03776@gmail.com>
2026-06-17 20:24:39 +0300

    v9

--- src/main/java/com/mo/MG_Movie/config/SecurityConfig.java
+++ src/main/java/com/mo/MG_Movie/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                         .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/movies/**").permitAll()
+                        .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
                 );
 
